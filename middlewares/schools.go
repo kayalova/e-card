d430e26af4be15480e0423044f8d96deea357bb8
Zarema <zarema.klva@gmail.com>
2020-08-17 03:41:43 +0300

    Add schools' handlers

--- middlewares/schools.go
+++ middlewares/schools.go
@@ -1,11 +1,56 @@
 package middlewares
 
 import (
+	"encoding/json"
 	"net/http"
-)
 
+	"github.com/kayalova/e-card-catalog/helpers"
+	"github.com/kayalova/e-card-catalog/postgres"
+
+	"github.com/kayalova/e-card-catalog/models"
+)
 
 // GetAllSchools return all schools
 func GetAllSchools(w http.ResponseWriter, r *http.Request) {
+	schools, err := getAllSchools()
 
+	if err != nil {
+		helpers.Error("Unable to get schools", http.StatusInternalServerError, w)
+		return
+	}
+
+	response, err := json.Marshal(schools)
+	if err != nil {
+		helpers.Error("Unable to get schools", http.StatusInternalServerError, w)
+		return
+	}
+
+	w.Write(response)
+
+}
+
+/* ------ postgres requests ------*/
+func getAllSchools() ([]models.School, error) {
+	db := postgres.CreateConnection()
+	defer db.Close()
+
+	sqlStatement := `SELECT * FROM schools`
+	var schools []models.School
+	rows, err := db.Query(sqlStatement)
+
+	if err != nil {
+		return schools, err
+	}
+
+	for rows.Next() {
+		var school models.School
+		err = rows.Scan(&school.ID, &school.Name)
+		if err != nil {
+			return schools, err
+		}
+
+		schools = append(schools, school)
+	}
+
+	return schools, nil
 }
